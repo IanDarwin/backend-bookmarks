c21d9a1683e76eb148b0a3d594b350489003e748
Ian Darwin <ian@darwinsys.com>
2021-03-10 16:33:50 -0500

    Better sample data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 insert into topic(id,name,description) values(1, 'econ', 'The economy');
 insert into topic(id,name,description) values(2, 'pol', 'Politics');
 
-insert into bookmark(id,topic_id, url, description) values(1, 1, 'http://huff.puff.com', 'Economy booming now that pandemic is done');
-insert into bookmark(id,topic_id, url, description) values(1, 1, 'http://huff.puff.com', 'Breaking: A politician told a lie!');
+insert into bookmark(id,topic_id, url, description) values(1, 1, 'http://huff.puff.com', 'The economy is booming!');
+insert into bookmark(id,topic_id, url, description) values(2, 2, 'http://huff.puff.com', 'Breaking: A politician told a lie!');
