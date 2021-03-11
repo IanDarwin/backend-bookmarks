e695ed9e7d1d0a491158a45ae72801dd2953c7d4
Ian Darwin <ian@darwinsys.com>
2021-03-10 20:26:36 -0500

    More sample data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,5 @@ insert into topic(id,name,description) values(2, 'pol', 'Politics');
 
 insert into bookmark(id,topic_id, url, description) values(1, 1, 'http://huff.puff.com', 'The economy is booming!');
 insert into bookmark(id,topic_id, url, description) values(2, 2, 'http://huff.puff.com', 'Breaking: A politician told a lie!');
+insert into bookmark(id,topic_id, url, description) values(3, 1, 'http://huff.puff.com', 'Some other country''s economy is not booming so much.');
+insert into bookmark(id,topic_id, url, description) values(4, 2, 'http://huff.puff.com', 'Breaking: Another politician told the truth!');
