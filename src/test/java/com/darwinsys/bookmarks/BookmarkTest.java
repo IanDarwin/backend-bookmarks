70903671d3f82727596d0a4989c104872fabd967
Ian Darwin <ian@darwinsys.com>
2021-03-11 21:13:38 -0500

    Rename files; revamp for this application

---
+++ src/test/java/com/darwinsys/bookmarks/BookmarkTest.java
@@ -0,0 +1,36 @@
+package com.darwinsys.quarkus;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.darwinsys.bookmarks.model.Bookmark;
+
+public class BookmarkTest {
+
+	final static String url = "https://telconews.com/foo";
+	final static String cut = "What was AT&T Thinking?";
+	Bookmark b;
+
+	@BeforeEach
+	public void setup() {
+        b = new Bookmark("att", url, cut);
+	}
+
+    @Test
+    public void testIdsAreZeroForNewObjs() {
+		assertEquals(0, b.getId());
+		assertEquals(0, b.getTopicId());
+	}
+	@Test
+	public void testGetMainFields() {
+		assertEquals("att", b.getTopicName());
+		assertEquals(url, b.getUrl());
+		assertEquals(cut, b.getDescription());
+    }
+	@Test
+	public void testAsHtml() {
+		assertEquals("<a href='" + url + "'>" + cut + "</a>", b.toHtml());
+	}
+
+}
