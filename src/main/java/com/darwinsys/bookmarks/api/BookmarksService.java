229d54a7b88ae60c81196a0a0e61c44fe51fc625
Ian Darwin <ian@darwinsys.com>
2021-03-26 15:30:02 -0400

    Don't keep API as own git/maven project; just incorporate it here.

---
+++ src/main/java/com/darwinsys/bookmarks/api/BookmarksService.java
@@ -0,0 +1,24 @@
+package com.darwinsys.bookmarks.api;
+
+import com.darwinsys.bookmarks.model.*;
+
+import java.util.*;
+
+import javax.persistence.*;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("")
+@Produces(MediaType.APPLICATION_JSON)
+public interface BookmarksService {
+	@GET@Path("topics")
+	public List<Topic> topics();
+	@GET@Path("bookmarksByTopicId/{topic}")
+	public List<Bookmark> bookmarksByTopicId(@PathParam("topic") long topic);
+	@GET@Path("bookmarksSearch/{pattern}")
+	public List<Bookmark> bookmarksSearch(@PathParam("pattern") String pattern);
+	@GET@Path("bookmarksRegex/{regex}")
+	public List<Bookmark> bookmarksRegex(@PathParam("regex") String regexString);
+	@GET@Path("bookmarkById/{id}")
+	public Bookmark bookmarkById(@PathParam("id") long id);
+}
