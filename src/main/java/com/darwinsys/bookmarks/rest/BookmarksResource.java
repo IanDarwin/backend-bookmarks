d4d828dc53831c7106294c8cf5ec8d2925efc42a
Ian Darwin <ian@darwinsys.com>
2022-12-26 10:01:54 -0500

    BookmarksResource: add save method; repair indentation

--- src/main/java/com/darwinsys/bookmarks/rest/BookmarksResource.java
+++ src/main/java/com/darwinsys/bookmarks/rest/BookmarksResource.java
@@ -8,64 +8,77 @@ import java.util.*;
 import java.util.regex.*;
 
 import javax.persistence.*;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("")
 public class BookmarksResource implements BookmarksService {
 
-	@PersistenceContext
-	EntityManager em;
+    @PersistenceContext
+    EntityManager em;
 
     @GET
-	@Path("topics")
+    @Path("topics")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Topic> topics() {
-		List<Topic> results = em.createQuery("from Topic").getResultList();
-        return results;
+        return em.createQuery("from Topic", Topic.class).getResultList();
     }
 
     @GET
-	@Path("bookmarksByTopicId/{topic}")
+    @Path("bookmarksByTopicId/{topic}")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Bookmark> bookmarksByTopicId(@PathParam("topic") long topic) {
-		List<Bookmark> results = em.
-			createQuery("from Bookmark where topic_id = ?1").
-			setParameter(1, topic).
-			getResultList();
+        return em.
+            createQuery("from Bookmark where topic_id = ?1", Bookmark.class).
+            setParameter(1, topic).
+            getResultList();
+    }
+
+    @GET
+    @Path("bookmarksSearch/{pattern}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Bookmark> bookmarksSearch(@PathParam("pattern") String pattern) {
+        var likePattern = String.format("%%%s%%", pattern.toLowerCase());
+        System.out.println("BookmarksResource::bookmarksBySearch(" + likePattern + ")");
+        return em.
+            createQuery("from Bookmark where description like ?1", Bookmark.class).
+            setParameter(1, likePattern).
+            getResultList();
+    }
+
+    @GET
+    @Path("bookmarksRegex/{regex}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Bookmark> bookmarksRegex(@PathParam("regex") String regexString) {
+        List<Bookmark> results = new ArrayList<>();
+        Pattern patt = Pattern.compile(regexString);
+        em.createQuery("from Bookmark", Bookmark.class).getResultList().
+            forEach(b -> { if (patt.matcher(b.getDescription()).find()) results.add(b); } );
         return results;
     }
 
     @GET
-	@Path("bookmarksSearch/{pattern}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Bookmark> bookmarksSearch(@PathParam("pattern") String pattern) {
-		var likePattern = String.format("%%%s%%", pattern.toLowerCase());
-		System.out.println("BookmarksResource::bookmarksBySearch(" + likePattern + ")");
-		return em.
-			createQuery("from Bookmark where description like ?1").
-			setParameter(1, likePattern).
-			getResultList();
-    }
-
-    @GET
-	@Path("bookmarksRegex/{regex}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Bookmark> bookmarksRegex(@PathParam("regex") String regexString) {
-		List<Bookmark> results = new ArrayList();
-		Pattern patt = Pattern.compile(regexString);
-		em.createQuery("from Bookmark", Bookmark.class).getResultList().
-			forEach(b -> { if (patt.matcher(b.getDescription()).find()) results.add(b); } );
-		return results;
-	}
-
-    @GET
-	@Path("bookmarkById/{id}")
+    @Path("bookmarkById/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Bookmark bookmarkById(@PathParam("id") long id) {
-		return em.find(Bookmark.class, id);
+        return em.find(Bookmark.class, id);
+    }
+
+    // Inserts into database and returns new pkey
+    @POST
+    @Path("bookmark")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public long addBookmark(Bookmark newBookmark) {
+        em.persist(newBookmark);
+        Bookmark inserted = em.merge(newBookmark);
+        return inserted.getId();
     }
 }
