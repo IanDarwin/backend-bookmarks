7990d57976eabc0340786a7bbac9b2142f9b9abb
Ian Darwin <ian@darwinsys.com>
2023-03-01 17:50:42 -0500

    equals and hashCode

--- src/main/java/com/darwinsys/bookmarks/model/Topic.java
+++ src/main/java/com/darwinsys/bookmarks/model/Topic.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 
 @Entity
 public class Topic {
+
 	@Id
 	String id;
 	String description;
@@ -28,5 +29,23 @@ public class Topic {
 
 	public String getDescription() {
 		return description;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Topic topic = (Topic) o;
+
+		if (!getId().equals(topic.getId())) return false;
+		return getDescription().equals(topic.getDescription());
+	}
+
+	@Override
+	public int hashCode() {
+		int result = getId().hashCode();
+		result = 31 * result + getDescription().hashCode();
+		return result;
 	}
 }
